4e8040ce4c0c3c684e7ac71bdccee7e4330ff815
abdul-aziz <tamimaziz2007@gmail.com>
2024-06-06 19:07:04 +0600

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document) .ready(function(){
     
  var $grid = $('.portfolio-active').isotope({
@@ -22,22 +21,4 @@ $(document) .ready(function(){
           event.preventDefault();
         });
 
-=======
-$(document) .ready(function(){
-    
- var $grid = $('.portfolio-active').isotope({
-        itemSelector: '.grid-item',
-        percentPosition: true,
-        masonry: {
-          // use outer width of grid-sizer for columnWidth
-          columnWidth: 1
-        }
-      })
-
-      $('.portfolio-menu').on( 'click', 'button', function() {
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-      });
-
->>>>>>> b507ac3db6caae960c03008fe03e393523fc9333
 })
